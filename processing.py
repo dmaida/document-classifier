f9f587a7a593e854d90d9c419e4148ad9258d1b0
Kevin Harris <k.michael7.harris8@gmail.com>
2017-04-10 10:16:38 -0700

    Added command line args functionality and some error handling

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 def processing(f_in):
 	"""
@@ -7,14 +8,25 @@ def processing(f_in):
 	2) removes all non ascii characters and replaces them with spaces
 	3) compresses all spaces to a single space
 	"""
+	
 	input_string = f_in.read() #read in
 	input_string = input_string.lower() #to lower case
 	input_string = re.sub("[^0-9a-z]+", ' ', input_string)
 	input_string = re.sub('\s+', ' ', input_string).strip() #compresses spaces
 	print(input_string)
-	
-def main():
-	print("I am main")
+
+def main(argv):
+	"""
+	Main method for running program
+	Interprets a single command line arg for text processing
+	"""
+	if len(argv) < 2:
+		print("Must provide a file as an argument")
+		return
+	if len(argv) > 2:
+		print("Must procide only one argument for processing")
+		return
+	processing(argv[1])
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv)
