e5090fd335bad46744193cbde487b8490faf098e
Kevin Harris <k.michael7.harris8@gmail.com>
2017-04-17 09:26:56 -0700

    Added partial skeleton code for bayes and intelli-grep

---
+++ bayes.py
@@ -0,0 +1,11 @@
+import processing
+import sys
+
+def bayes():
+    pass
+
+def main(argv):
+    print(argv)
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ grep.py
@@ -0,0 +1,15 @@
+import processing
+import sys
+
+def search():
+    """
+    Search method for classifying a document based on number of times
+    a phrase appears
+    """
+    print('test')
+
+def main(argv):
+    print(argv)
+
+if __name__ == '__main__':
+    main(sys.argv)
