a67b61025c4dee15356b404fd56c88caae1ba4d3
Kevin Harris <k.michael7.harris8@gmail.com>
2017-04-18 19:22:08 -0700

    Creates a list of tuples of each of the top 20 words seen in each training set.  Still need to add pickle functionality if we don't want to run this multiple times

--- create_feature.py
+++ create_feature.py
@@ -2,19 +2,25 @@ import processing
 import pickle
 import os
 import sys
+from collections import Counter
 
 def get_frequency(path, file_list):
 	words = ''
 	for f in file_list:
-		words += processing.processing(os.path.join(base_path, f))
+		words += processing.processing(os.path.join(path, f))
 	words = words.split() #splits into list on whitespace
 	total_num_words = len(words)
+	word_dict = dict(Counter(words))
+	frequency_list = list({w: c/total_num_words for w,c in word_dict.items()}.items())
+	#print(frequency_list)
+	sorted_freq_list = sorted(frequency_list, key=lambda x: x[1], reverse=True)
+	return sorted_freq_list[0:20]
 
 def main(argv):
-	base_path = argv
-	print(argv)
 	if len(argv) != 2:
 		base_path = 'data'
+	else:
+		base_path = argv[1]
 	dr_path = os.path.join(base_path,'DR')
 	dt_path = os.path.join(base_path, 'DT')
 	l_path = os.path.join(base_path, 'L')
@@ -23,9 +29,12 @@ def main(argv):
 	dt_files = [f for f in os.listdir(dt_path) if os.path.isfile(os.path.join(dt_path, f))]
 	l_files = [f for f in os.listdir(l_path) if os.path.isfile(os.path.join(l_path, f))]
 
-	dr_word_list = get_words(dr_path, dr_files)
-	dt_word_list = get_words(dt_path, dt_files)
-	l_word_list = get_words(l_path, l_files)
-	
+	dr_freq = get_frequency(dr_path, dr_files)
+	dt_freq = get_frequency(dt_path, dt_files)
+	l_freq = get_frequency(l_path, l_files)
+	print(dr_freq)
+	print(dt_freq)
+	print(l_freq)
+
 if __name__ == '__main__':
 	main(sys.argv)
