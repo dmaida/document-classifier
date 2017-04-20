e0a3eb7e405506c65127aba508291240e2f7207d
Joscha Oswald <joschaoswald@gmail.com>
2017-04-20 00:17:06 -0700

    Updated create_feature to create a dictionary of the training documents, then be able to create a feature set based off that dictionary. so the files only have to be processed once

--- bayes.py
+++ bayes.py
@@ -1,13 +1,26 @@
-import create_feature as featureCreator
+import create_feature
 import sys
 
 def naive_bayes(training_path):
-    bag_of_words = featureCreator.create_navie_boolean_feature_set(training_path)
+    bag_of_words = create_feature.create_navie_boolean_feature_set(training_path)
 
 
 def main(argv):
     print(argv)
 
 if __name__ == '__main__':
-    featureCreator.create_navie_boolean_feature_set('data')
+    #featureCreator.create_navie_boolean_feature_set('data')
+    """
+    docDict = create_feature.create_naive_document_dictionaries_from_training_files('data')
+    create_feature.get_frequency_from_training_documents(docDict)
+    """
+    #testing Comparing the two methods
+    documents = create_feature.create_naive_document_dictionaries_from_training_files('data')
+    bag_of_words = create_feature.create_boolean_feature_set(documents)
+    print(bag_of_words)
+    print('lenght:', len(bag_of_words))
+    print('OLD WAY')
+    new_bag_of_words = create_feature.file_create_naive_boolean_feature_set('data')
+    print(new_bag_of_words)
+    print("length", len(new_bag_of_words))
     #main(sys.argv)
