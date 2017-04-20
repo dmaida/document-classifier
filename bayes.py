7bb84dc7ef648844c34e7359dad059e104eb8f66
Joscha Oswald <joschaoswald@gmail.com>
2017-04-19 23:15:44 -0700

    Added create_feature_set and renamed some of the functions

--- bayes.py
+++ bayes.py
@@ -1,11 +1,13 @@
-import processing
+import create_feature as featureCreator
 import sys
 
-def bayes():
-    pass
+def naive_bayes(training_path):
+    bag_of_words = featureCreator.create_navie_boolean_feature_set(training_path)
+
 
 def main(argv):
     print(argv)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    featureCreator.create_navie_boolean_feature_set('data')
+    #main(sys.argv)
--- create_feature.py
+++ create_feature.py
@@ -4,6 +4,14 @@ import os
 import sys
 from collections import Counter
 
+"""Creates a boolean bag of words feature set"""
+def create_navie_boolean_feature_set(training_path):
+	dr_freq, dt_freq, l_freq = get_naive_frequency_from_training_files(training_path)
+	#combines the lists
+	feature_words = [ x[0] for x in dr_freq ] + [ x[0] for x in dt_freq ] + [ x[0] for x in l_freq ]
+	#The set gets rid of duplicates, then it makes a dictionary for a bag for word
+	return dict.fromkeys(set(feature_words))
+
 def get_frequency(path, file_list):
 	words = ''
 	for f in file_list:
@@ -15,12 +23,17 @@ def get_frequency(path, file_list):
 	#print(frequency_list)
 	sorted_freq_list = sorted(frequency_list, key=lambda x: x[1], reverse=True)
 	return sorted_freq_list[0:20]
+def create_document_dictionaries_from_training_files(base_path):
 
-def main(argv):
-	if len(argv) != 2:
-		base_path = 'data'
-	else:
-		base_path = argv[1]
+	dr_files = [f for f in os.listdir(dr_path) if os.path.isfile(os.path.join(dr_path, f))]
+	dt_files = [f for f in os.listdir(dt_path) if os.path.isfile(os.path.join(dt_path, f))]
+	l_files = [f for f in os.listdir(l_path) if os.path.isfile(os.path.join(l_path, f))]
+	dr_documents = dt_documents = l_documents = {}
+	for f in dr_files:
+		dr_documents[f] = processing.processing(os.path.join(base_path,f))
+
+def get_naive_frequency_from_training_files(base_path):
+
 	dr_path = os.path.join(base_path,'DR')
 	dt_path = os.path.join(base_path, 'DT')
 	l_path = os.path.join(base_path, 'L')
@@ -35,6 +48,13 @@ def main(argv):
 	print(dr_freq)
 	print(dt_freq)
 	print(l_freq)
+	return [dr_freq, dt_freq, l_freq]
 
+def main(agrv):
+	if len(argv) != 2:
+		base_path = 'data'
+	else:
+		base_path = argv[1]
+	get_naive_frequency_from_training_files(base_path)
 if __name__ == '__main__':
 	main(sys.argv)
