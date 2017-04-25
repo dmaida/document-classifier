d049e6f187f09be1379e19a57b7f203dc956839e
Kevin Harris <k.michael7.harris8@gmail.com>
2017-04-24 20:13:01 -0700

    Able to train perceptron.  Working on training 1 right now, adjusting weights and bias as needed

--- perceptron.py
+++ perceptron.py
@@ -11,10 +11,10 @@ class perceptron():
 	def __init__(self, doc_class, feature_set):
 		self.doc_class = doc_class
 		self.feature_set = {}
-		self.bias = 0.5
+		self.bias = 1
 		self.alpha = 0.1
 		for word in feature_set:
-			self.feature_set[word] = 0.5
+			self.feature_set[word] = 1
 
 	def _freqs(self, word_list):
 		"""
@@ -27,17 +27,45 @@ class perceptron():
 			doc_freq[key] = c/num_words
 		return doc_freq
 
+
 	def _training(self, training_set):
 		"""
 
 		"""
+		tot_false_neg = 0
+		tot_false_pos = 0
 		in_score = 0
-		shuffle(training_set) #randomly shuffles the training set.  Should happen every iteration
-		for doc in training_set:
-			doc_data = word_bag(doc[0])
-			freq = self._freqs(doc_data)
-			print(freq)
-			break
+		for i in range(10):
+			shuffle(training_set) #randomly shuffles the training set.  Should happen every iteration
+			iter_neg = 0
+			iter_pos = 0
+			for doc in training_set:
+				#print(doc, self.doc_class)
+				doc_data = word_bag(doc[0])
+				freq = self._freqs(doc_data)
+				for f in freq:
+					in_score += freq[f]*self.feature_set[f]
+				if in_score > 0 and doc[1] != self.doc_class: #false positive
+					#print('False positive')
+					tot_false_pos += 1
+					iter_pos += 1
+					self.bias += self.alpha*-1
+					for word in self.feature_set:
+						if freq[f] != 0: self.feature_set[word] += self.alpha*-1
+				elif in_score <= 0 and doc[1] == self.doc_class: #false negative
+					#print('False negative')
+					tot_false_neg += 1
+					iter_neg += 1
+					self.bias += self.alpha*1
+					for word in self.feature_set:
+						if freq[f] != 0: self.feature_set[word] += self.alpha*1
+			print('Done with iteration ', i)
+			print('Iteration false negative: ', iter_neg)
+			print('Iteration false positive: ', iter_pos)
+			print('Total false: ', iter_pos+iter_neg)
+			self.alpha -= 0.001
+		print('Total false negatives: ', tot_false_neg)
+		print('Total false positives: ', tot_false_pos)
 
 def word_bag(f_path):
 	"""
@@ -64,15 +92,14 @@ def create_training_set():
 		
 	return training_list
 
-def main(argv):
-    print(argv)
+def main():
     path = 'data'
     documents = create_feature.create_naive_document_dictionaries_from_training_files(path)
     features = list(dict.fromkeys(create_feature.create_boolean_feature_set(documents) ))
     training_set = create_training_set()
-    print(training_set[0])
+    print(len(training_set))
     p = perceptron('DR',features) #DT perceptron
     p._training(training_set)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
